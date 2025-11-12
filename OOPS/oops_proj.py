c6658374cb71a4d114e6dcba8066020b34de1c14
Viveka Sharma <vivekasharma01@gmail.com>
2025-11-12 21:23:57 +0530

    signup and signin is done

--- OOPS/oops_proj.py
+++ OOPS/oops_proj.py
@@ -10,15 +10,15 @@ class chatbook:
         #user_input = input("Welcome to  Chatbook ! How you would like to proceed? \n 1. Press 1 to Login \n 2. Signup \n 3. Press 3 to write a post"")    
         
         user_input =input('''Welcome to  Chatbook ! How you would like to proceed? 
-                          1. Press 1 to Login 
+                          1. Press 1 to Signup 
                           2. Press 2 to Signin
                           3. Press 3 to Write a post
                           4. Press 4 to Message a friend
-                          5. Press Any other key to Logout''')
-        if user_input=='1':  #string format 
-            pass
+                          5. Press Any other key to Logout   ''')
+        if user_input=='1':  #here 1 is taken in string format 
+            self.signup()
         elif user_input=='2':
-            pass 
+            self.signin() 
         elif user_input=='3':
             pass
         elif user_input=='4':
@@ -26,6 +26,34 @@ class chatbook:
         else:
             print("You have been logged out. Goodbye!")
             exit()
-
+            
+    def signup(self):
+        email = input("Enter your email : ")
+        pwd = input("Setup your password : ")
+        self.username = email  
+        self.password = pwd 
+        print("Signup successful! You can now Signin.") 
+        print("\n")
+        self.menu()
+    
+    def signin(self):
+        '''if not self.username or not self.password:
+            print("No user found. Please signup first.")
+            self.menu()
+            return '''
+            
+        if self.username=="" or self.password=="":
+            print("No user found. Please signup first by selecting Pressing 1 in the main menu.")
+        else:
+            uname = input("Enter your email : ") 
+            pwd = input("Enter your password : ")
+            if self.username==uname and self.password==pwd:
+                print("Signin successful! Welcome back.")
+                self.loggedin=True 
+            else:
+                print("Enter correct credentials. Please try again.")    
+        print("\n")      
+        self.menu()
+            
 #create object/instance of the class            
 obj = chatbook()                 
